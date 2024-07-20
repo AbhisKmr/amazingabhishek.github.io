72100fd473b560ea9356acee752e702db29ea1d6
AbhisKmr <akaks09@gmail.com>
2024-07-21 03:20:55 +0530

    init repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box w="100vw" h="100vh" background="black">
+    </Box>
+  );
+}
+
+export default App;
